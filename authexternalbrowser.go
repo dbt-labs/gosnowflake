fd6b57228cdc9129634fbdbeb7acb5cca7e7d5b8
Mila Page <67295367+VersusFacit@users.noreply.github.com>
2025-05-26 16:58:04 -0700

    Update authexternalbrowser.go
    Use single line comments per style recommendation
    
    Co-authored-by: Felipe Oliveira Carvalho <felipekde@gmail.com>

--- authexternalbrowser.go
+++ authexternalbrowser.go
@@ -268,8 +268,8 @@ func doAuthenticateByExternalBrowser(
 	defer l.Close()
 
 	callbackPort := l.Addr().(*net.TCPAddr).Port
-	/* prevent infinite hang if the user doesn't open the browser or never completes the redirect
-	Apparently net.Listener.Accept() does not observe ctx cancelation by default */
+	// prevent infinite hang if the user doesn't open the browser or never completes the redirect
+	// Apparently net.Listener.Accept() does not observe ctx cancelation by default
 	_ = l.SetDeadline(time.Now().Add(5 * time.Minute))
 
 	var loginURL string
